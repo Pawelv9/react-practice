ec0c4c1d8903ea4d9093159706a568fb3868d193
Pavel <Pawelv9@gmail.com>
2020-04-07 19:42:20 +0300

    Initial commit

---
+++ src/components/PageNotFound.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const PageNotFound = () => <h1>Page is not exists in this universe</h1>
+
+export default PageNotFound;
---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutPage = () => (
+
+  <div>
+    <h2>About</h2>
+    <p>Some valuable content</p>
+  </div>
+
+);
+
+export default AboutPage;
